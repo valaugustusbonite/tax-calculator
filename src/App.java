131d081ef221052d457374ccff354f5bb6bc9b57
val.bonite <val.bonite@whitecloak.com>
2023-05-04 21:45:28 +0800

    Implement Sales Tax Calculator

---
+++ src/App.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        TaxCalculator taxCalculator = new TaxCalculator(new SalesTaxCalculator());
+        
+        Scanner sc =new Scanner(System.in);  
+
+        System.out.print("How much was your sales?");  
+        double sales = sc.nextDouble();  
+
+        System.out.print("Which state will you be paying from? Please input the postal abbreviation (eg. 'NY' for New York).");  
+        String state = sc.next();  
+
+        double tax = taxCalculator.calculateTax(sales, state);
+
+        System.out.println("You will need to pay a total amount of $" + tax + " to the state of " + state + "!");
+
+        sc.close();
+    }
+}
+
+interface TaxCalculationStrategy {
+    public double calculateTax(double amount, String stateAbbr) throws Exception;
+}
+
+ class SalesTaxCalculator implements TaxCalculationStrategy {
+
+    @Override
+    public double calculateTax(double amount, String stateAbbr) throws Exception {
+        double taxRate = USStateTaxRates.getStateTaxRate(stateAbbr);
+
+        return amount * taxRate;
+    }
+}
+
+class TaxCalculator {
+    public TaxCalculationStrategy calculationStrategy;
+
+    TaxCalculator(TaxCalculationStrategy calculationStrategy) {
+        this.calculationStrategy = calculationStrategy;
+    }
+
+    public double calculateTax(double amount, String stateAbbr) throws Exception {
+        return calculationStrategy.calculateTax(amount, stateAbbr);
+    }
+}
+
+class USStateTaxRates {
+    private static Map<String, Double> taxRateTable;
+
+    static {
+        taxRateTable = new HashMap<>();
+
+        taxRateTable.put("MN", 0.0678);
+        taxRateTable.put("CA", 0.065);
+        taxRateTable.put("NY", 0.04);
+        taxRateTable.put("NM", 0.05125);
+        taxRateTable.put("TX", 0.0625);
+    }
+
+    public static void addState(String stateAbbr, double taxRate) {
+        taxRateTable.putIfAbsent(stateAbbr, taxRate);
+    }
+
+    public static void updateTaxRateOfState(String stateAbbr, double newRate) throws Exception {
+        if (!taxRateTable.containsKey(stateAbbr)) {
+            throw new Exception("Can't update tax rate because state " + stateAbbr + " does not exist.");
+        }
+
+        taxRateTable.put(stateAbbr, newRate);
+    }
+
+    public static double getStateTaxRate(String stateAbbr) throws Exception {
+        if (!taxRateTable.containsKey(stateAbbr)) {
+            throw new Exception("No record of state " + stateAbbr);
+        }
+
+        return taxRateTable.get(stateAbbr);
+    }
+
+    public static void removeState(String stateAbbr) throws Exception {
+        if (!taxRateTable.containsKey(stateAbbr)) {
+            throw new Exception("Can't delete state. No record of state " + stateAbbr);
+        }
+
+        taxRateTable.remove(stateAbbr);
+    }
+
+    public static ArrayList<String> getAllStateAbbreviations() {
+        return new ArrayList<>(taxRateTable.keySet());
+    }
+
+    public static ArrayList<Double> getAllStateTaxRates() {
+        return new ArrayList<>(taxRateTable.values());
+    }
+}
+
+
